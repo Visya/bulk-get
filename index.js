12701f9758d1f46842d611ac64166c2d3be10b49
Mariia Verbenko <mariia.verbenko@harmonicinc.com>
2016-09-19 04:09:40 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var _ = require('lodash');
+var fetch = require('request');
+var jsonStream = require('JSONStream');
+var path = require('path');
+var pi = require('pipe-iterators');
+
+module.exports = (request, response, next) => {
+  if (_.isEmpty(request.query)) {
+    return next();
+  }
+
+  let streams = _.map(request.query, jsonGetterFor(request, next));
+  let keys = _.keys(request.query);
+
+  pi.merge(streams)
+  .pipe(pi.reduce((result, item) => _.extend(result, item)))
+  .pipe(jsonStream.stringify(false))
+  .pipe(response);
+}
+
+function fullPathFor(request, url) {
+  return 'http://' + path.normalize(request.headers.host + '/' + url);
+} 
+
+function jsonGetterFor(request, next) {
+  return (url, key) => fetch.get(fullPathFor(request, url))
+	  .on('error', (error) => next(error))
+	  .pipe(jsonStream.parse().on('error', (error) => next(error)));
+}
